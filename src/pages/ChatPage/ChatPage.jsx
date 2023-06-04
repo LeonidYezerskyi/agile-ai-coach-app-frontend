11517c31e6225e47fc71862ccc26ef64a85e15eb
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-06-04 03:15:11 +0300

    ADDED: stylies for ChatPage

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -3,20 +3,28 @@ import css from "./ChatPage.module.css";
 
 const ChatPage = () => {
     return (
-        <Box className={css.container}>
+        <div className={css.container}>
             <div className={css.content}>
-                <div className={css.contentWrapper1}>
-                    <p className={css.message1}>Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos</p>
+                <div className={css.dialogWrapper}>
+                    <div className={css.contentWrapper1}>
+                        <p className={css.txt}>Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos</p>
+                    </div>
+                    <div className={css.contentWrapper2}>
+                        <p className={css.txt}>Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos</p>
+                    </div>
+                    <div className={css.contentWrapper1}>
+                        <p className={css.txt}>Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos</p>
+                    </div>
+                    <div className={css.contentWrapper1}>
+                        <p className={css.txt}>Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos</p>
+                    </div>
                 </div>
-                <div className={css.contentWrapper2}>
-                    <p className={css.message2}>Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos</p>
-                </div>
-                <div>
+                <div className={css.inputWrapper}>
                     <input type="text" placeholder="Type your message..." className={css.input} />
                     <button className={css.btn}>Send</button>
                 </div>
             </div>
-        </Box>
+        </div>
     )
 }
 
